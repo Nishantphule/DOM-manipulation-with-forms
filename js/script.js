0e95badfab4494b4af82f5905ab102d04ba848c3
Nishantphule <nishantphule12@gmail.com>
2022-10-20 11:31:44 +0530

    Dom manipulation with forms

---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict"
+// creating a function for button submit 
+// when u click on submit button it will collect the data
+
+document.getElementById("submit").addEventListener("click", function nishant() {
+  let fnameo = document.getElementById("first-name").value;
+  let lnameo = document.getElementById("last-name").value;
+  let addresso = document.getElementById("address").value;
+  let pincodeo = document.getElementById("pincode").value;
+  let gendero = document.getElementsByName("gender");
+  
+  // to get the select radio button value
+  if (gendero[0].checked) {
+    gendero = gendero.value = "MALE"
+  }
+  else if (gendero[1].checked) {
+    gendero = gendero.value = "FEMALE"
+  }
+  else if (gendero[2].checked){
+    gendero = gendero.value = "OTHER"
+  }
+  else {
+    gendero = gendero.value = "Not selected"
+  }
+  
+  let food1o = document.getElementById("food1").value;
+  let food2o = document.getElementById("food2").value;
+  let stateo = document.getElementById("state").value;
+  let countryo = document.getElementById("country").value;
+  
+  const data = {
+    fname: fnameo,
+    lname: lnameo,
+    address: addresso,
+    pincode: pincodeo,
+    gender: gendero,
+    food: food1o + "<br>" + food2o,
+    state: stateo,
+    country: countryo
+  }
+  
+  let dataTable = "";
+  dataTable += "<tr>";
+  dataTable += "<td>" + data.fname + "</td>";
+  dataTable += "<td>" + data.lname + "</td>";
+  dataTable += "<td>" + data.address + "</td>";
+  dataTable += "<td>" + data.pincode + "</td>";
+  dataTable += "<td>" + data.gender + "</td>";
+  dataTable += "<td>" + data.food + "</td>";
+  dataTable += "<td>" + data.state + "</td>";
+  dataTable += "<td>" + data.country + "</td>";
+  document.getElementById('data').innerHTML += dataTable;
+  form.reset()
+}
+)
+
+
+
+  
